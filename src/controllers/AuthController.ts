6ab1892a3a7312eb8f41fbd2756b22c154310403
Ndrolp <ndro.lozano@gmail.com>
2025-05-18 18:50:57 +0200

    feat: update ndro-express-utils and add validation
    Use Joi to validate endpoints

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,18 @@
-import { Controller, Route } from "ndro-express-utils";
+import { Controller, Route, Validate } from "ndro-express-utils";
 import { LoginBody } from "../types/auth.types";
 import { AuthService } from "../services/AuthService";
 import { Request, Response } from "express";
+import Joi from "joi";
+
+const loginValidationSchema = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+});
 
 @Controller("/auth")
 export default class AuthController {
     @Route("post", "/login")
+    @Validate(loginValidationSchema)
     async login(req: Request<{}, {}, LoginBody>, res: Response) {
         const { username, password } = req.body as {
             username: string;
